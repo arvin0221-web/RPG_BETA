885a5a17e208c5cbe7f8ac233a9e8c5868269215
arvin0221-web <arvin0221@apps.ntpc.edu.tw>
2026-01-11 15:48:29 +0800

    Update app.js

--- app.js
+++ app.js
@@ -618,7 +618,7 @@ function buyWand(i) {
   player.weapons.push(newWand);
     // ✅ 新增：購買成功後存檔
   if (typeof saveGameExtended === "function") {
-    saveGameExtended(false);
+    saveGameExtended();
   }
   showGlobalTip(`你獲得了 ${newWand.name}（${rarity}）`);
   updateUI();
