7b65601efb489f269693ce4e278c58134ba4292a
arvin0221-web <arvin0221@apps.ntpc.edu.tw>
2026-01-26 14:55:07 +0800

    Update vip.js

--- vip.js
+++ vip.js
@@ -3,23 +3,19 @@
  *************************************************/
 (function() {
   const VIP_NAME = "3x-27y=5π";
+  const VIP_NAMEE = "菜菜";
   const MULTIPLIER = 50;
+  const MULTIPLIERR = 10;
 
   // 如果是 VIP 玩家，直接修改怪物屬性
   if (player.name === VIP_NAME) {
     monster.expGain *= MULTIPLIER;
     monster.gold *= MULTIPLIER;
   }
-
-})();
-(function() {
-  const VIP_NAMEE = "菜菜";
-  const MULTIPLIERR = 10;
-
-  // 如果是 VIP 玩家，直接修改怪物屬性
   if (player.name === VIP_NAMEE) {
     monster.expGain *= MULTIPLIERR;
     monster.gold *= MULTIPLIERR;
   }
+  
 
 })();
